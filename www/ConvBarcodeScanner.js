41a1e00925d76837b8b9b24a878272bf7d7a2056
Charles Grimont <grimont.c@gmail.com>
2019-04-03 18:31:22 +0200

    resolve bug of double quotes in cordova.js

--- www/ConvBarcodeScanner.js
+++ www/ConvBarcodeScanner.js
@@ -14,10 +14,10 @@ module.exports = {
 			}
 		}, 
 		function(result){
-			console.error("JSON Error ConvBarcodeScanner"); 
+			console.error('JSON Error ConvBarcodeScanner'); 
 		},
-		"ConvBarcodeScanner",
-		"Scan",parametersAsArray(parameters));
+		'ConvBarcodeScanner',
+		'Scan',parametersAsArray(parameters));
 	}
 
 	
@@ -26,20 +26,4 @@ module.exports = {
 parametersAsArray = (parameters)=> {
 	let array = [parameters];
 	return array;
-	/*if(parameters == undefined){
-		return null;
-	}
-	else{
-		parameters["laserColor"] != undefined ? array.push(parameters["laserColor"]) : array.push(null);
-		parameters["laserEnabled"] != undefined ? array.push(parameters["laserEnabled"]) : array.push(null);
-		parameters["maskColor"] != undefined ? array.push(parameters["maskColor"]) : array.push(null);
-		parameters["maskOpacity"] != undefined ? array.push(parameters["maskOpacity"]) : array.push(null);
-		parameters["textDown"] != undefined ? array.push(parameters["maskColor"]) : array.push(null);
-		parameters["textUp"] != undefined ? array.push(parameters["maskColor"]) : array.push(null);
-		parameters["squareEnabled"] != undefined ? array.push(parameters["maskColor"]) : array.push(null);
-		parameters["borderColor"] != undefined ? array.push(parameters["maskColor"]) : array.push(null);
-		parameters["textUpColor"] != undefined ? array.push(parameters["maskColor"]) : array.push(null);
-		parameters["textDownColor"] != undefined ? array.push(parameters["maskColor"]) : array.push(null);
-		return array;
-	}*/
 };
